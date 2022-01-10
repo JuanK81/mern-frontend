657b97ecf915cd91919bbc4bb66e17d455be754c
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-10 17:16:20 +0100

    use openstreetmap instead of google maps

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-import "./Maps.css";
+import "./Map.css";
+
+
+
+//lat: 39.56739530702399,
+//lng: 2.6482925241340376,
+// https://blog.logrocket.com/react-leaflet-tutorial/
+// https://react-leaflet.js.org/docs/example-react-control/
 
 const Map = (props) => {
-  return <div className={`map ${props.className}`} style={props.style}>
-      
-  </div>;
+    
+  return (
+    <div
+      className={`leaflet-container map ${props.className}`}
+      style={props.style}
+    >
+      <MapContainer
+        // className="leaflet-container"
+        style={{ height: 320, width: 625 }}
+        center={[39.56739530702399, 2.6482925241340376]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[39.56739530702399, 2.6482925241340376]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
