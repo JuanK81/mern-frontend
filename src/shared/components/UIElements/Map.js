9b012ffe8956b69b7648dea738eacec79d51cc98
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-10 16:31:30 +0100

    add map component, map provider to be chosen

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Maps.css";
+
+const Map = (props) => {
+  return <div className={`map ${props.className}`} style={props.style}>
+      
+  </div>;
+};
+
+export default Map;
