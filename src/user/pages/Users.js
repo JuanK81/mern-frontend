23df3be6fa717087d22d41827ec98427aa23c9b2
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-19 18:39:01 +0100

    start connection with back-end

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import UsersList from "../components/UsersList";
+import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-      id: "u1",
+      id: 'u1',
       image:
-        "https://images.pexels.com/photos/3175971/pexels-photo-3175971.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-      name: "Mixteca",
-      placeCount: "3",
+        'https://images.pexels.com/photos/3175971/pexels-photo-3175971.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      name: 'Mixteca',
+      placeCount: '3',
     },
   ];
 
