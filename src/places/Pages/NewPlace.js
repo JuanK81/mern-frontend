f67c84550a478d4c693208cb72363c6d7a1ca3df
JuanK81 <jcasalsbadolato@gmail.com>
2022-01-21 18:27:39 +0100

    connect DB, back and front, handle files and storage.

--- src/places/Pages/NewPlace.js
+++ src/places/Pages/NewPlace.js
@@ -2,67 +2,70 @@ import React, { useContext, Fragment } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from '../../shared/components/util/validators';
-import Button from '../../shared/components/FormElements/Button';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
-import ErrorModal from '../../shared/components/UIElements/ErrorModal';
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import './PlaceForm.css';
 
-const NewPLace = () => {
+console.log('my code');
+const NewPlace = () => {
   const auth = useContext(AuthContext);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const [formState, inputHandler] = useForm({
-    title: {
-      value: '',
-      isValid: false,
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+      address: {
+        value: '',
+        isValid: false,
+      },
+      image: {
+        value: null,
+        isValid: false,
+      },
     },
-    description: {
-      value: '',
-      isValid: false,
-    },
-    address: {
-      value: '',
-      isValid: false,
-    },
-  });
+    false
+  );
 
   const history = useHistory();
 
   const placeSubmitHandler = async (event) => {
     event.preventDefault();
-    
+
     try {
-      await sendRequest(
-        'http://localhost:5000/api/places',
-        'POST',
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId
-        }),
-        {
-          'Content-Type': 'application/json'
-        }
-      );
+      const formData = new FormData();
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('creator', auth.userId);
+      console.log(formState.inputs.image.value);
+      formData.append('image', formState.inputs.image.value);
+      await sendRequest('http://localhost:5000/api/places', 'POST', formData);
       history.push('/');
     } catch (err) {
       console.log(err);
     }
   };
 
-  console.log('clg formstate inputs', formState.inputs);
   return (
     <Fragment>
       <ErrorModal error={error} onClear={clearError} />
       <form className="place-form" onSubmit={placeSubmitHandler}>
-      {isLoading && <LoadingSpinner asOverlay />}
+        {isLoading && <LoadingSpinner asOverlay />}
         <Input
           id="title"
           type="text"
@@ -75,6 +78,7 @@ const NewPLace = () => {
         <Input
           id="description"
           type="textarea"
+          element="textarea"
           label="Description"
           rows="5"
           validators={[VALIDATOR_MINLENGTH(5)]}
@@ -90,6 +94,11 @@ const NewPLace = () => {
           errorText="Please enter a valid address."
           onInput={inputHandler}
         />
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          error="Please provide an image."
+        />
         <Button type="submit" disabled={!formState.isValid}>
           ADD PLACE
         </Button>
@@ -98,4 +107,96 @@ const NewPLace = () => {
   );
 };
 
-export default NewPLace;
+export default NewPlace;
+
+
+
+
+
+
+// console.log('max code');
+// const NewPlace = () => {
+//   const auth = useContext(AuthContext);
+//   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+//   const [formState, inputHandler] = useForm(
+//     {
+//       title: {
+//         value: '',
+//         isValid: false,
+//       },
+//       description: {
+//         value: '',
+//         isValid: false,
+//       },
+//       address: {
+//         value: '',
+//         isValid: false,
+//       },
+//       image: {
+//         value: null,
+//         isValid: false,
+//       },
+//     },
+//     false
+//   );
+
+//   const history = useHistory();
+
+//   const placeSubmitHandler = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const formData = new FormData();
+//       formData.append('title', formState.inputs.title.value);
+//       formData.append('description', formState.inputs.description.value);
+//       formData.append('address', formState.inputs.address.value);
+//       formData.append('creator', auth.userId);
+//       formData.append('image', formState.inputs.image.value);
+//       await sendRequest('http://localhost:5000/api/places', 'POST', formData);
+//       history.push('/');
+//     } catch (err) {}
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <ErrorModal error={error} onClear={clearError} />
+//       <form className="place-form" onSubmit={placeSubmitHandler}>
+//         {isLoading && <LoadingSpinner asOverlay />}
+//         <Input
+//           id="title"
+//           element="input"
+//           type="text"
+//           label="Title"
+//           validators={[VALIDATOR_REQUIRE()]}
+//           errorText="Please enter a valid title."
+//           onInput={inputHandler}
+//         />
+//         <Input
+//           id="description"
+//           element="textarea"
+//           label="Description"
+//           validators={[VALIDATOR_MINLENGTH(5)]}
+//           errorText="Please enter a valid description (at least 5 characters)."
+//           onInput={inputHandler}
+//         />
+//         <Input
+//           id="address"
+//           element="input"
+//           label="Address"
+//           validators={[VALIDATOR_REQUIRE()]}
+//           errorText="Please enter a valid address."
+//           onInput={inputHandler}
+//         />
+//         <ImageUpload
+//           id="image"
+//           onInput={inputHandler}
+//           errorText="Please provide an image."
+//         />
+//         <Button type="submit" disabled={!formState.isValid}>
+//           ADD PLACE
+//         </Button>
+//       </form>
+//     </React.Fragment>
+//   );
+// };
+
+// export default NewPlace;
